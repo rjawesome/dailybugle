2176dd8a8750a53cc62a4c8a97dc89aa61d62e4e
strangiato2112 <jskaplan2004@gmail.com>
2021-09-01 14:10:30 -0800

    updated title

--- main.py
+++ main.py
@@ -47,6 +47,16 @@ def Jason():
 def sussy():
     return render_template("sophie.html")
 
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("greet.html", name=name)
+    # starting and empty input default
+    return render_template("greet.html", name="World")
+
 
 # runs the application on the development server
 if __name__ == "__main__":
