eb5876e9a79dd8aca19682200cf31598d167047d
PunarvasuS <punarvasu.sangam@gmail.com>
2021-09-01 14:29:10 -0700

    Added punnu.html in main.py

--- main.py
+++ main.py
@@ -43,9 +43,14 @@ def stub2():
 @app.route('/Jason/')
 def Jason():
     return render_template("Jason.html")
+
 @app.route('/Sophie')
 def sussy():
     return render_template("sophie.html")
+
+@app.route('/Punnu/')
+def Punnu():
+    return render_template("punnu.html")
 
 @app.route('/greet', methods=['GET', 'POST'])
 def greet():
