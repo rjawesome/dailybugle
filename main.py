d53a4e108f7ce5168087ba8bb351894124be718e
strangiato2112 <jskaplan2004@gmail.com>
2021-09-01 14:37:04 -0800

    greet and updates

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create a Flask instance
 app = Flask(__name__)
@@ -47,15 +47,15 @@ def Jason():
 def sussy():
     return render_template("sophie.html")
 
-@app.route('/greet', methods=['GET', 'POST'])
+@app.route('/Jason/greet/', methods=['GET', 'POST'])
 def greet():
     # submit button has been pushed
     if request.form:
         name = request.form.get("name")
         if len(name) != 0:  # input field has content
-            return render_template("greet.html", name=name)
+            return render_template("Jason.html", name=name)
     # starting and empty input default
-    return render_template("greet.html", name="World")
+    return render_template("Jason.html", name="World")
 
 
 # runs the application on the development server
