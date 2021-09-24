ef96c52ad61dbc4285f7ec69680568b8035aabbb
CalrethilOfMirkwood <82109882+CalrethilOfMirkwood@users.noreply.github.com>
2021-09-23 22:42:03 -0700

    generalize the broken '/static/img' line

--- image.py
+++ image.py
@@ -20,7 +20,7 @@ def image_formatter(img, img_type):
 
 
 # color_data prepares a series of images for data analysis
-def image_data(path="C:/Users/guoet/IdeaProjects/flask_project/static/img/", img_list=None):  # path of static images is defaulted
+def image_data(path="/static/img/", img_list=None):  # path of static images is defaulted
     if img_list is None:  # color_dict is defined with defaults
         img_list = [
             {'source': "Peter Carolin", 'label': "Lassen Volcano", 'file': "lassen-volcano-256.jpg"},
